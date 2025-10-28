19b2753f39f69faf23a2b37209e63a01befcd7cc
steveacab <tony_not@hotmail.it>
2025-10-28 11:54:41 +0100

    Add files via upload

---
+++ cockpit-2fa/cockpit-2fa-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+USER=${SUDO_USER:-$(whoami)}
+SECRET_FILE="/home/$USER/.google_authenticator"
+
+if [ ! -f "$SECRET_FILE" ]; then
+    sudo -u $USER google-authenticator -t -f -d -r 3 -R 30 -w 3 -q > /dev/null
+fi
+
+SECRET=$(head -n 1 "$SECRET_FILE")
+URL="otpauth://totp/$USER@$(hostname)?secret=$SECRET&issuer=Cockpit"
+
+# Genera il QR code e restituisci direttamente il base64
+qrencode -t PNG -o - "$URL" | base64 -w 0
